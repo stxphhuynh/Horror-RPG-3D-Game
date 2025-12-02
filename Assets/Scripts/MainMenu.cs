0889cc7d067dbff00b1895110d36f76d48a6bbaf
Julian Stinnett <julian.stinnett34@gmail.com>
2025-12-02 10:17:47 -0800

    Menu/Weapon Scene
    added functionality to menu scene and starting on weapons menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         // name of gameplay scene
-        SceneManager.LoadScene(gameSceneName);
+        SceneManager.LoadScene(1);
         
     }
     public void QuitGame()
