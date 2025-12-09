abdbf3551ebdb8d3d7adba785ebff1f887ce305c
Julian Stinnett <julian.stinnett34@gmail.com>
2025-12-09 14:07:54 -0800

    added sprinting audio

--- Assets/Scripts/footsteps.cs
+++ Assets/Scripts/footsteps.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class footsteps : MonoBehaviour
 {
-    public AudioSource footstepsSound;
+    public AudioSource footstepsSound;  // walking
+    public AudioSource runningSound;    // running
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-        {
-        
+        bool moving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) ||
+                      Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
 
-            footstepsSound.enabled = true;
-            //sprintSound.enabled = true;
+        bool running = Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W);
+
+        if (moving)
+        {
+            if (running)
+            {
+                // RUNNING: enable running sound, disable walking
+                runningSound.enabled = true;
+                footstepsSound.enabled = false;
+            }
+            else
+            {
+                // WALKING: enable walking sound, disable running
+                footstepsSound.enabled = true;
+                runningSound.enabled = false;
+            }
         }
         else
         {
+            // STOPPED: both off
             footstepsSound.enabled = false;
-            //sprintSound.enabled = false;
+            runningSound.enabled = false;
         }
     }
 }
